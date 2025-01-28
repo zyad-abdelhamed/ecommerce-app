ffe06d15cf8b74eae69ea699d66ca61a3e2ee091
solimanesammosa <esam.mousa.6576@gmail.com>
2025-01-28 17:07:58 +0200

    refactor

--- lib/features/payment_integration/presentation/views/components/payment1_body.dart
+++ lib/features/payment_integration/presentation/views/components/payment1_body.dart
@@ -5,7 +5,6 @@ import 'package:ecommerce_application/core/utils/sized_boxs.dart';
 import 'package:ecommerce_application/core/widgets/app_material_button.dart';
 import 'package:ecommerce_application/core/widgets/app_sneak_bar.dart';
 import 'package:ecommerce_application/features/payment_integration/data/models/payment_intent_input_model.dart';
-import 'package:ecommerce_application/features/payment_integration/data/models/payment_intent_model/amount_details.dart';
 import 'package:ecommerce_application/features/payment_integration/data/models/paypal_model/amount.dart';
 import 'package:ecommerce_application/features/payment_integration/data/models/paypal_model/item.dart';
 import 'package:ecommerce_application/features/payment_integration/data/models/paypal_model/item_list.dart';
@@ -76,46 +75,52 @@ class Payment1Body extends StatelessWidget {
                   StripeCubit cubit = context.read<StripeCubit>();
                   return appMaterialButton(
                       buttonFunction: () {
-                        // cubit.makePayment(
-                        //     paymentIntentInputModel: PaymentIntentInputModel(
-                        //         amount: totalPrice, currency: 'USD'));
-                        var paypalModel = PaypalModel(
-                            amount: Amount(
-                                currency: 'USD',
-                                details: Details(
-                                    shipping: '0',
-                                    shippingDiscount: 0,
-                                    subtotal: '70'),
-                                total: '70'),
-                            description: 'The payment transaction description.',
-                            itemList: ItemList(items: [
-                              Item(
+                        if (state.activeButton == 2) {
+                          cubit.makePayment(
+                              paymentIntentInputModel: PaymentIntentInputModel(
+                                  amount: totalPrice, currency: 'USD'));
+                        } else {
+                          var paypalModel = PaypalModel(
+                              amount: Amount(
                                   currency: 'USD',
-                                  name: 'soliman',
-                                  price: '30',
-                                  quantity: 3)
-                            ]));
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) => PaypalCheckoutView(
-                            sandboxMode: true,
-                            clientId: "AakWTi_MtaqAwF1aL5dX60GORfm1Rro-maHj_uv0EjYvcJbIkRP5Q6ZoRX5jOTreId-xFs0POW4Ikacr",
-                            secretKey: "EHg3u4JRpDWvPPQRV7zZbv64_ru1bmjzQmG9xzzh789ydhO-bEvQDya5fGmrqjLVcskPQKOulnFLwTQA",
-                            transactions:  [
-                              paypalModel.toJson()
-                            ],
-                            note: "Contact us for any questions on your order.",
-                            onSuccess: (Map params) async {
-                              print("onSuccess: $params");
-                            },
-                            onError: (error) {
-                              print("onError: $error");
-                              Navigator.pop(context);
-                            },
-                            onCancel: () {
-                              print('cancelled:');
-                            },
-                          ),
-                        ));
+                                  details: Details(
+                                      shipping: '0',
+                                      shippingDiscount: 0,
+                                      subtotal: '90'),
+                                  total: '90'),
+                              description:
+                                  'The payment transaction description.',
+                              itemList: ItemList(items: [
+                                Item(
+                                    currency: 'USD',
+                                    name: 'soliman',
+                                    price: '30',
+                                    quantity: 3)
+                              ]));
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                PaypalCheckoutView(
+                              sandboxMode: true,
+                              clientId:
+                                  "AakWTi_MtaqAwF1aL5dX60GORfm1Rro-maHj_uv0EjYvcJbIkRP5Q6ZoRX5jOTreId-xFs0POW4Ikacr",
+                              secretKey:
+                                  "EHg3u4JRpDWvPPQRV7zZbv64_ru1bmjzQmG9xzzh789ydhO-bEvQDya5fGmrqjLVcskPQKOulnFLwTQA",
+                              transactions: [paypalModel.toJson()],
+                              note:
+                                  "Contact us for any questions on your order.",
+                              onSuccess: (Map params) async {
+                                print("onSuccess: $params");
+                              },
+                              onError: (error) {
+                                print("onError: $error");
+                                Navigator.pop(context);
+                              },
+                              onCancel: () {
+                                print('cancelled:');
+                              },
+                            ),
+                          ));
+                        }
                       },
                       buttonName: state.stripeRequestStateEnum ==
                               RequestStateEnum.loading
